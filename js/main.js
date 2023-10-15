b790ece482cc313e97c6274f98f38173297c99b6
josephmisheal <145793357+josephmisheal@users.noreply.github.com>
2023-10-16 01:30:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 
 
-function onFormSubmit() {
-//culclate salary and added and decleration data
+    var name = document.getElementById("Name").value;
+    var title = document.getElementById("Title").value;
     var HourRate = parseFloat(document.getElementById("HourRate").value);
     var Hour = parseFloat(document.getElementById("Hour").value);
     var salary = HourRate * Hour + ".LE";
-    if (Hour>=24){
+function onFormSubmit() {
+//culclate salary and added and decleration data
+    var name = document.getElementById("Name").value;
+    var title = document.getElementById("Title").value;
+    var HourRate = parseFloat(document.getElementById("HourRate").value);
+    var Hour = parseFloat(document.getElementById("Hour").value);
+
+    if (name === "" || title === "") {
+        alert("Please fill all data correctly.");
+        return false;}
+    else if(isNaN(HourRate) || isNaN(Hour)){
+        alert("Input must be a number");
+        return false;
+    }
+
+    var salary = HourRate * Hour + ".LE";
+    if (Hour>24){
         alert ("You have exceeded the number of hours in a day");
         return;
     }
     var formData = readFormData();
     formData.salary = salary;
-    addDataToTable(formData);}
-   
+    addDataToTable(formData);
+    
+}
+
 
 //put data in array opject to select it and take a valueto read it 
     function readFormData() {
         var formData = {};
-        formData["name"] = document.getElementById("name").value;
+        formData["Name"] = document.getElementById("Name").value;
         formData["Title"] = document.getElementById("Title").value;
         return formData;
     }
@@ -31,23 +49,23 @@ function addDataToTable(person) {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
 //data in table 
-    cell1.innerHTML = person.name;
+    cell1.innerHTML = person.Name;
     cell2.innerHTML = person.salary;
     cell3.innerHTML = person.Title;
-    cell4.innerHTML = " <button onclick='deleteRow(this)'>Delete</button>";
-    cell4.innerHTML += "<button onclick='editRow(this)'>Edit</button>";
+    cell4.innerHTML = " <button class='btn btn-outline-danger' onclick='deleteRow(this)'id='Delete' >Delete</button>";
+    cell4.innerHTML += "<button class='btn btn-outline-info ml-1' onclick='editRow(this)'  id='edit' >Edit</button>";
+
 }
 
 function editRow(button) {
     var row = button.parentNode.parentNode;
     var salaryCell = row.cells[1];
-    
     // update salary
     var newSalary = prompt("Enter the new salary:");
 
     //to get new value of newsalary
     if (newSalary !== null) {
-        salaryCell.innerHTML = newSalary;
+        salaryCell.innerHTML = newSalary+".LE";
     }
 }
 // delete row
